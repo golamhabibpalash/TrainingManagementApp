86bca6d2cede93dad3dc572273d1080d63dba720
Golam Habib Palash <38590071+golamhabibpalash@users.noreply.github.com>
2020-03-29 09:32:35 +0600

    first commit

---
+++ TrainingManagementApp/TrainingManagementApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TrainingManagementApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
